51f3193369e78133f51c27ae66c1093da4d4b3d5
Ritik <rvjaat25@gmail.com>
2023-12-14 10:53:55 +0530

    first-commit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+  }
+  const Preload = document.getElementById("preload")
+setTimeout(() => {
+  document.body.classList.remove("overflow-hidden");
+  Preload.classList.add("d-none")
+}, "5000");
